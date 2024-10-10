a3e5d62d9ff826e53e5f78a1d7bc0de0ec584740
Josh MacDonald <rockiesgrizzly@icloud.com>
2024-10-10 14:03:58 -0600

    cleanup

--- Sources/Networking/Request.swift
+++ Sources/Networking/Request.swift
@@ -9,23 +9,24 @@ import Combine
 import Foundation
 
 /// Protocol enables injection of test URLSession objects
-protocol URLSessionProtocol {
-    func data(from url: URL, delegate: (any URLSessionTaskDelegate)?) async throws -> (Data, URLResponse)
+protocol URLSessionAsycProtocol {
+    func data(for request: URLRequest, delegate: (any URLSessionTaskDelegate)?) async throws -> (Data, URLResponse)
 }
 
-extension URLSession : URLSessionProtocol { }
 
-class Request<Model:Decodable> {
+extension URLSession : URLSessionAsycProtocol { }
+
+class Request<Model: Decodable & Sendable> {
     
-    private var streamTask: Task<(), Error>?
+    // MARK: - Async/Await
 
     /// This function takes the provided `requestModel` and utilizes
     /// URLSession to retrieve the desired `ResponseModel`.
     /// - Parameter requestModel: `RequestModel` offers various parameters. This function defaults to
     /// `cachePolicy` : `.reloadIgnoringLocalAndRemoteCacheData` & `timeoutInterval` 0 if not provided in `requestModel`
     /// - Returns: any object conforming to `Decodable`
-    public static func asyncGet(from url: URL, session: any URLSessionProtocol = URLSession.shared) async throws -> Model? {
-        let (data, response) = try await session.data(from: url, delegate: nil)
+    public static func asyncGet(_ request: URLRequest, session: any URLSessionAsycProtocol = URLSession.shared) async throws -> Model? {
+        let (data, response) = try await session.data(for: request, delegate: nil)
         
         guard let httpResponse = response as? HTTPURLResponse,
               httpResponse.statusCode == 200 else {
@@ -34,54 +35,7 @@ class Request<Model:Decodable> {
         
         return try JSONDecoder().decode(Model.self, from: data)
     }
-    
-    
-    /// This function creates a throwing stream of generic type `Model`on a time interval
-    /// - Parameters:
-    ///   - url: endpoint from which to retrieve
-    ///   - repeatTimeInterval: desired time interval stream should return
-    /// - Returns: generic model requested
-    public func asyncThrowingStream(from url: URL, repeatTimeInterval: TimeInterval, session: any URLSessionProtocol = URLSession.shared) async -> AsyncThrowingStream<Model, Error> {
-        return AsyncThrowingStream<Model, Error> { continuation in
-            streamTask = Task {
-                do {
-                    try await withThrowingTaskGroup(of: Model?.self) { group in
-                        repeat {
-                            guard !Task.isCancelled else { throw RequestError.taskCancelled }
-                            
-                            if let model = try await Request.asyncGet(from: url, session: session) {
-                                continuation.yield(model)
-                            }
-                            
-                            try await Task.sleep(nanoseconds: UInt64(repeatTimeInterval * 1_000_000_000))
-                        } while !Task.isCancelled
-                    }
-                } catch {
-                    continuation.finish(throwing: error)
-                }
-            }
-        }
-    }
-    
-    public func cancelAsyncThrowingStreamTask() {
-        streamTask?.cancel()
-    }
 
-    // MARK: - Combine
-    
-    public static func repeatingPublisher(from url: URL, interval: TimeInterval, session: URLSession = URLSession.shared) -> AnyPublisher<Model, Error> {
-        return Timer.publish(every: interval, on: .main, in: .common)
-            .autoconnect()
-            .flatMap { _ in
-                return session.dataTaskPublisher(for: url)
-                    .tryMap { result -> Model in
-                        return try JSONDecoder().decode(Model.self, from: result.data)
-                    }
-                    .receive(on: RunLoop.main)
-            }
-            .eraseToAnyPublisher()
-    }
-    
     // MARK: - Errors
     public enum RequestError: Error {
         case httpUrlResponseFailed(URLResponse)
--- Tests/NetworkingTests/RequestTests.swift
+++ Tests/NetworkingTests/RequestTests.swift
@@ -13,71 +13,30 @@ import Foundation
 
 
 final class NetworkingTests {
-    let url = URL(string: "https://jsonplaceholder.typicode.com/todos/1")!
-    let session = TestURLSession()
+    static let url = URL(string: "https://jsonplaceholder.typicode.com/todos/1")!
+    static let request = URLRequest(url: url)
+    static let session = TestURLSession()
     
     @Test("Request.asyncGet Success")
-    func asyncGetSuccess() async throws {
-        let model: MockModel? = try await Request<MockModel>.asyncGet(from: url, session: session)
+    static func asyncGetSuccess() async throws {
+        let model: MockModel? = try await Request<MockModel>.asyncGet(request, session: session)
         #expect(model != nil)
         #expect(model?.id == 1)
         #expect(model?.name == "delectus aut autem")
     }
     
     @Test("Request.asyncGet Failure")
-    func asyncGetFailure() async throws {
-        let model: MockModel? = try await Request<MockModel>.asyncGet(from: url, session: session)
+    static func asyncGetFailure() async throws {
+        let model: MockModel? = try await Request<MockModel>.asyncGet(request, session: session)
         #expect(model?.id != 2)
         #expect(model?.name != "lorum ipsum")
     }
-    
-    @Test("Request.asyncThrowingStream Success")
-    func testAsyncThrowingStreamSuccess() async throws {
-        let stream = try #require(await Request<MockModel>().asyncThrowingStream(from: url, repeatTimeInterval: 0.1, session: session))
-        
-        var receivedMocks = [MockModel]()
-        try await confirmation(expectedCount: 1) { confirmation in
-            for try await mockModel in stream {
-                receivedMocks.append(mockModel)
-                
-                if receivedMocks.count == 2  {
-                    confirmation()
-                    break
-                }
-            }
-        }
-    }
-    
-    // TODO: Test stream cancellation. Fairly impossible in XCTest do to a Task not allowing an outside parameter. To cancel a task, one would need some way to see the results. Explore Swift Testing in next Xcode for expanded concurrenncy testing options
-    
-    private var cancellables: Set<AnyCancellable> = []
-    
-    //    @Test("RepeatingPublisher produces correct value")
-    //    func repeatingPublisherProducesCorrectValue() {
-    //        let interval: TimeInterval = 0.1
-    //        let expectation = expectation(description: "Publisher emits values")
-    //
-    //        var receivedModels = [MockModel]()
-    //        Request<MockModel>.repeatingPublisher(from: url, interval: interval, session: session)
-    //            .collect(2) // Collect first 2 emissions
-    //            .sink(receiveCompletion: { _ in }, receiveValue: { models in
-    //                receivedModels = models
-    //                expectation.fulfill()
-    //            })
-    //            .store(in: &cancellables)
-    //
-    //        wait(for: [expectation], timeout: 0.5)
-    //
-    //        // Assertions using #expect from the Testing framework
-    //        #expect(receivedModels.count == 2)
-    //        #expect(receivedModels[0] == MockModel(id: modelId, name: modelString))
-    //        #expect(receivedModels[1] == MockModel(id: modelId, name: modelString))
-    //    }
 }
+
+// MARK: - Test Models
 
 private let modelId = 1
 private let modelString = "delectus aut autem"
-
 
 // Sample Model struct for testing
 struct MockModel: Codable, Equatable {
@@ -85,15 +44,36 @@ struct MockModel: Codable, Equatable {
     let name: String
 }
 
-struct TestURLSession: URLSessionProtocol {
+struct TestURLSession: URLSessionAsycProtocol {
     static let model = MockModel(id: modelId, name: modelString)
-    let data = try! JSONEncoder().encode(model)
-    static let url = URL(string: "u2.com")!
-    let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: ["Content-Type": "application/json"])!
+    var data = try! JSONEncoder().encode(model)
+    static let url = URL(string: "u2.com")! // unused
+    static let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: ["Content-Type": "application/json"])!
     
-    func data(from url: URL, delegate: (any URLSessionTaskDelegate)?) async throws -> (Data, URLResponse) {
-        return (data, response)
+    func data(for request: URLRequest, delegate: (any URLSessionTaskDelegate)?) async throws -> (Data, URLResponse) {
+        return (data, TestURLSession.response)
     }
+}
+
+class URLCombineProtocol: URLProtocol {
+    @MainActor static var response: URLResponse? = TestURLSession.response
+    @MainActor static var error: Error?
+    
+    override class func canInit(with request: URLRequest) -> Bool { true }
+    override class func canInit(with task: URLSessionTask) -> Bool { true }
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest { request }
+    
+    override func startLoading() {
+        let session = TestURLSession()
+        
+        self.client?.urlProtocol(self, didLoad: session.data)
+        self.client?.urlProtocol(self,
+                                 didReceive: TestURLSession.response,
+                                 cacheStoragePolicy: .notAllowed)
+        self.client?.urlProtocolDidFinishLoading(self)
+    }
+
+    override func stopLoading() { }
 }
 
 
