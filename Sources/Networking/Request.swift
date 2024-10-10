b68c080545af08c8db1e3ba262e8e8b568a4276a
Josh MacDonald <rockiesgrizzly@icloud.com>
2024-10-10 08:58:59 -0600

    unit testing

--- Sources/Networking/Request.swift
+++ Sources/Networking/Request.swift
@@ -5,19 +5,27 @@
 //  Created by joshmac on 8/30/24.
 //
 
+import Combine
 import Foundation
+
+/// Protocol enables injection of test URLSession objects
+protocol URLSessionProtocol {
+    func data(from url: URL, delegate: (any URLSessionTaskDelegate)?) async throws -> (Data, URLResponse)
+}
+
+extension URLSession : URLSessionProtocol { }
 
 class Request<Model:Decodable> {
     
     private var streamTask: Task<(), Error>?
-    
+
     /// This function takes the provided `requestModel` and utilizes
     /// URLSession to retrieve the desired `ResponseModel`.
     /// - Parameter requestModel: `RequestModel` offers various parameters. This function defaults to
     /// `cachePolicy` : `.reloadIgnoringLocalAndRemoteCacheData` & `timeoutInterval` 0 if not provided in `requestModel`
     /// - Returns: any object conforming to `Decodable`
-    public static func asyncGet(from url: URL) async throws -> Model? {
-        let (data, response) = try await URLSession.shared.data(from: url)
+    public static func asyncGet(from url: URL, session: any URLSessionProtocol = URLSession.shared) async throws -> Model? {
+        let (data, response) = try await session.data(from: url, delegate: nil)
         
         guard let httpResponse = response as? HTTPURLResponse,
               httpResponse.statusCode == 200 else {
@@ -28,12 +36,12 @@ class Request<Model:Decodable> {
     }
     
     
-    /// <#Description#>
+    /// This function creates a throwing stream of generic type `Model`on a time interval
     /// - Parameters:
-    ///   - url: <#url description#>
-    ///   - repeatTimeInterval: <#repeatTimeInterval description#>
-    /// - Returns: <#description#>
-    public func asyncThrowingStream(from url: URL, repeatTimeInterval: TimeInterval) async -> AsyncThrowingStream<Model, Error> {
+    ///   - url: endpoint from which to retrieve
+    ///   - repeatTimeInterval: desired time interval stream should return
+    /// - Returns: generic model requested
+    public func asyncThrowingStream(from url: URL, repeatTimeInterval: TimeInterval, session: any URLSessionProtocol = URLSession.shared) async -> AsyncThrowingStream<Model, Error> {
         return AsyncThrowingStream<Model, Error> { continuation in
             streamTask = Task {
                 do {
@@ -41,7 +49,7 @@ class Request<Model:Decodable> {
                         repeat {
                             guard !Task.isCancelled else { throw RequestError.taskCancelled }
                             
-                            if let model = try await Request.asyncGet(from: url) {
+                            if let model = try await Request.asyncGet(from: url, session: session) {
                                 continuation.yield(model)
                             }
                             
@@ -58,9 +66,26 @@ class Request<Model:Decodable> {
     public func cancelAsyncThrowingStreamTask() {
         streamTask?.cancel()
     }
+
+    // MARK: - Combine
     
+    public static func repeatingPublisher(from url: URL, interval: TimeInterval, session: URLSession = URLSession.shared) -> AnyPublisher<Model, Error> {
+        return Timer.publish(every: interval, on: .main, in: .common)
+            .autoconnect()
+            .flatMap { _ in
+                return session.dataTaskPublisher(for: url)
+                    .tryMap { result -> Model in
+                        return try JSONDecoder().decode(Model.self, from: result.data)
+                    }
+                    .receive(on: RunLoop.main)
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    // MARK: - Errors
     public enum RequestError: Error {
         case httpUrlResponseFailed(URLResponse)
         case taskCancelled
     }
 }
+
--- Tests/NetworkingTests/RequestTests.swift
+++ Tests/NetworkingTests/RequestTests.swift
@@ -1,43 +1,104 @@
-import XCTest
+//
+//  RequestTests.swift
+//
+//
+//  Created by joshmac on 8/30/24.
+//
+
+import Testing
 @testable import Networking
 
-final class NetworkingTests: XCTestCase {
+import Combine
+import Foundation
+
+
+final class NetworkingTests {
     let url = URL(string: "https://jsonplaceholder.typicode.com/todos/1")!
+    let session = TestURLSession()
     
-    struct MockModel: Codable {
-        let id: Int
-        let title: String
+    @Test("Request.asyncGet Success")
+    func asyncGetSuccess() async throws {
+        let model: MockModel? = try await Request<MockModel>.asyncGet(from: url, session: session)
+        #expect(model != nil)
+        #expect(model?.id == 1)
+        #expect(model?.name == "delectus aut autem")
     }
     
-    func testAsyncGetSuccess() async throws {
-        let model: MockModel? = try await Request<MockModel>.asyncGet(from: url)
-        XCTAssertNotNil(model)
-        XCTAssert(model?.id == 1)
-        XCTAssert(model?.title == "delectus aut autem")
+    @Test("Request.asyncGet Failure")
+    func asyncGetFailure() async throws {
+        let model: MockModel? = try await Request<MockModel>.asyncGet(from: url, session: session)
+        #expect(model?.id != 2)
+        #expect(model?.name != "lorum ipsum")
     }
     
-    func testWrongUrlFails() async throws {
-        let failedURL = URL(string: "https://www.u2.com")!
-        let model: MockModel? = try? await Request<MockModel>.asyncGet(from: failedURL)
-        XCTAssertNil(model)
-    }
-    
+    @Test("Request.asyncThrowingStream Success")
     func testAsyncThrowingStreamSuccess() async throws {
-        let expectation = XCTestExpectation(description: "Successful async fetch")
-        let stream = await Request<MockModel>().asyncThrowingStream(from: url, repeatTimeInterval: 0.1)
+        let stream = try #require(await Request<MockModel>().asyncThrowingStream(from: url, repeatTimeInterval: 0.1, session: session))
         
         var receivedMocks = [MockModel]()
-        for try await mockModel in stream {
-            receivedMocks.append(mockModel)
-            
-            if receivedMocks.count == 2  {
-                expectation.fulfill()
-                break
+        try await confirmation(expectedCount: 1) { confirmation in
+            for try await mockModel in stream {
+                receivedMocks.append(mockModel)
+                
+                if receivedMocks.count == 2  {
+                    confirmation()
+                    break
+                }
             }
         }
-        
-        await fulfillment(of: [expectation], timeout: 1)
     }
     
     // TODO: Test stream cancellation. Fairly impossible in XCTest do to a Task not allowing an outside parameter. To cancel a task, one would need some way to see the results. Explore Swift Testing in next Xcode for expanded concurrenncy testing options
+    
+    private var cancellables: Set<AnyCancellable> = []
+    
+    //    @Test("RepeatingPublisher produces correct value")
+    //    func repeatingPublisherProducesCorrectValue() {
+    //        let interval: TimeInterval = 0.1
+    //        let expectation = expectation(description: "Publisher emits values")
+    //
+    //        var receivedModels = [MockModel]()
+    //        Request<MockModel>.repeatingPublisher(from: url, interval: interval, session: session)
+    //            .collect(2) // Collect first 2 emissions
+    //            .sink(receiveCompletion: { _ in }, receiveValue: { models in
+    //                receivedModels = models
+    //                expectation.fulfill()
+    //            })
+    //            .store(in: &cancellables)
+    //
+    //        wait(for: [expectation], timeout: 0.5)
+    //
+    //        // Assertions using #expect from the Testing framework
+    //        #expect(receivedModels.count == 2)
+    //        #expect(receivedModels[0] == MockModel(id: modelId, name: modelString))
+    //        #expect(receivedModels[1] == MockModel(id: modelId, name: modelString))
+    //    }
 }
+
+private let modelId = 1
+private let modelString = "delectus aut autem"
+
+
+// Sample Model struct for testing
+struct MockModel: Codable, Equatable {
+    let id: Int
+    let name: String
+}
+
+struct TestURLSession: URLSessionProtocol {
+    static let model = MockModel(id: modelId, name: modelString)
+    let data = try! JSONEncoder().encode(model)
+    static let url = URL(string: "u2.com")!
+    let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: ["Content-Type": "application/json"])!
+    
+    func data(from url: URL, delegate: (any URLSessionTaskDelegate)?) async throws -> (Data, URLResponse) {
+        return (data, response)
+    }
+}
+
+
+
+
+
+
+
